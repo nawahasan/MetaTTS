200ead3ec8338fd60b8f09c9f7017f0624e72140
nawahasan <nawab.hasan@gmail.com>
2024-04-07 00:02:47 +0530

    Create setup.sh
    Commit

---
+++ setup.sh
@@ -0,0 +1,21 @@
+#@title Automatic Setup
+%pwd
+git clone https://github.com/jaywalnut310/vits.git
+python --version
+%cd vits/
+
+pip install Cython==0.29.21
+pip install librosa==0.8.0
+pip install phonemizer==2.2.1
+pip install scipy
+pip install "numpy<1.24"
+pip install torch
+pip install torchvision
+pip install matplotlib
+pip install Unidecode==1.1.1
+
+%cd monotonic_align/
+%mkdir monotonic_align
+python3 setup.py build_ext --inplace
+%cd ../
+%pwd
